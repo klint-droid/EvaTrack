7026651d922287ff5c83cc6052567d4417756231
Klint Ruales <klintruales11@gmail.com>
2026-04-29 21:55:57 +0800

    fix login bug

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\API\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum');
+//Route::post('/login', [AuthController::class, 'login']);
+//Route::post('/logout', [AuthController::class, 'logout'])
+//    ->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'currentUser']);
+//Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'currentUser']);
