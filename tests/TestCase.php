432c5fdd8988d8deaf21dfcfc2e5190ce3253783
Klint Ruales <klintruales11@gmail.com>
2026-06-05 02:08:31 +0800

    redirect mysql_v2 database connection to sqlite for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        // Redirect mysql_v2 connection to SQLite in-memory during tests
+        config(['database.connections.mysql_v2' => config('database.connections.sqlite')]);
+
+        parent::setUp();
+    }
 }
